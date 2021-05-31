3e23daed7ed3e63b70f41ee075ee05c609855934
mikios34 <haylemikaeltefera@gmail.com>
2021-05-31 14:56:03 +0300

    enque stylesheet

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function load_stylesheets(){
+    wp_register_style('stylesheet', get_template_directory_uri().'/style.css','',1,'all');
+    wp_enqueue_style('stylesheet');
+
+}
+
+add_Action('wp_enqueue_scripts','load_stylesheets');
