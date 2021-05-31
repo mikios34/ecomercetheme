0c5e30fb3c8ff70c286019bb96a05998e7f94493
Netsasew <netsasew55@gmail.com>
2021-05-31 22:06:46 +0300

    page.php creat

---
+++ page.php
@@ -0,0 +1,23 @@
+<?php get_header();?>
+
+
+<div class="content">
+    <div class="container">
+
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="sticky-top" style="top:50px">
+                <?php get_sidebar();?>
+            </div>
+            
+        </div>
+        
+    </div>
+    
+
+        
+    </div>
+    
+</div>
+
+<?php get_footer();?>
