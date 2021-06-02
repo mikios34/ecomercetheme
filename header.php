0ca96c6dfc12b0cb8c976675a0b741c02ca46ef1
mikios34 <haylemikaeltefera@gmail.com>
2021-06-02 14:20:16 +0300

    header modified

--- header.php
+++ header.php
@@ -5,9 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php
-    wp_head();
-    ?>
+    <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class('test')?>>
+<header>
+    <div class="container d-flex align-items-center justify-content-between">
+        
+        <img src="<?php bloginfo('template_directory');?>/assets/images/divider-purple.png" class="img-fluid logo">
+        <?php wp_nav_menu(
+            array(
+                'theme_location'=>'top-menu',
+                'menu_class'=>'top-menu d-flex'
+            )
+        )?>
+       
+       
+
+        
+    </div>
     
+</header>
