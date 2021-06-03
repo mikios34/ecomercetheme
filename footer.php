f3cbd7c08b9d430ce652003757164efdb0bddd7d
mikios34 <haylemikaeltefera@gmail.com>
2021-06-03 15:56:49 +0300

    fotter page modified with four widgitable areas

--- footer.php
+++ footer.php
@@ -1,7 +1,27 @@
-<footer>
-    <div class="container d-flex align-items-center justify-content-center h-100">
-        <p>Theme Development</p>
-    </div>
+<footer class='site-footer'>
+<div class="footer_widgets">
+    <?php if(is_active_sidebar( 'footer-area1' )):?>
+        <div class="footer-widget-area">
+            <?php dynamic_sidebar( 'footer-area1' )?>
+        </div>
+    <?php endif; ?>
+    <?php if(is_active_sidebar( 'footer-area2' )):?>
+        <div class="footer-widget-area">
+            <?php dynamic_sidebar( 'footer-area2' )?>
+        </div>
+    <?php endif; ?>
+    <?php if(is_active_sidebar( 'footer-area3' )):?>
+        <div class="footer-widget-area">
+            <?php dynamic_sidebar( 'footer-area3' )?>
+        </div>
+    <?php endif; ?>
+    <?php if(is_active_sidebar( 'footer-area4' )):?>
+        <div class="footer-widget-area">
+            <?php dynamic_sidebar( 'footer-area4' )?>
+        </div>
+    <?php endif; ?>
+</div>
+    <p>E-commerce Theme Develpment - &copy; <?php echo date('Y'); ?></p>
 </footer>
 <?php wp_footer();?>
 </body>
