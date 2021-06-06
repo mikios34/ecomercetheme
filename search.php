5f0888c21ddcb4798a64db2655c02ea9381a97c6
Henock-Yonas <henockyeyoni@gmail.com>
2021-06-07 04:55:27 +0900

    search

---
+++ search.php
@@ -0,0 +1,36 @@
+<?php get_header() ?>
+
+<main class="container-fluid py-5">
+        <div class="row">
+            <div class="col-sm-8">
+                <?php    if( have_posts()) : while( have_posts()): the_post();?>
+                <div>
+                    <a href="<?php the_permalink(); ?>">
+                    <h2><?php the_title();?></h2></a>
+                    <p><?php echo get_the_date('F j, Y'); ?> by  <a href=""> <?php the_author();?></a></p>
+                    <div class="pb-2">
+                        <p class="d-inline"><?php the_tags('Tagged: ', ' ~ '); ?></p>
+                    </div>
+                    <p>
+                    <div class="my-3">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                        <?php the_excerpt ()?></p>
+                        <div class="mb-3">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php  _e('Read more', 'velvet');?>
+                            </a>
+                        </div>
+                </div>
+                <?php
+                        endwhile; else: ?>
+ 
+  
+                <?php get_sidebar();?>
+            </aside>
+        </div>
+
+    </main>
+
+
+    <?php get_footer() ?>
